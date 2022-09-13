e7bb4070d799b92d168180499f22153e99bf3a2f
Luis Sergio <tectualonline@gmail.com>
2022-09-13 12:27:51 -0300

    Nova forma de modular os arquivos js

--- js/index.js
+++ js/index.js
@@ -1,33 +1,22 @@
-const routes = {
-    "/": "/pages/home.html",
-    "/about": "/pages/about.html",
-    "/contact": "/pages/contact.html",
-    404: "/pages/404.html",
-  }
+import {Router} from'./router.js'
+
+const router = new Router()
+router.add("/", "/pages/home.html")
+router.add("/about", "/pages/about.html")
+router.add("/contact", "/pages/contact.html")
+router.add(404, "/pages/404.html")
+
+// const routes = {
+//     "/": "/pages/home.html",
+//     "/about": "/pages/about.html",
+//     "/contact": "/pages/contact.html",
+//     404: "/pages/404.html",
+//   }
 
 
-  function route(event) {
-    event = event || window.event
-    event.preventDefault()
+ router.handle()
 
-    window.history.pushState({}, "", event.target.href)
+ 
 
-    handle()
-
-  }
-
-  function handle() {
-    const {pathname} = window.location
-    const route = routes[pathname] || routes[404]
-    fetch(route)
-      .then(data => data.text()) 
-      .then(html => {
-          document.querySelector('#app').innerHTML = html
-      })           
-   
-  }
-
-  handle()
-
-  window.onpopstate = () => handle()
-  window.route = () => route()
+  window.onpopstate = () => router.handle()
+   window.route = () => router.route()
